3de6542c74d504274d708918b7551c34f0a27144
Gary Burgess <gary.burgess@gmail.com>
2014-03-20 14:55:53 +0000

    Avoid unnecessary eta abstraction, fix `aceil`

--- src/Math.purs
+++ src/Math.purs
@@ -1,24 +1,12 @@
 module Math where
 
-foreign import abs
-  "function abs(n){\
-  \  return Math.abs(n);\
-  \}" :: Number -> Number
+foreign import abs "var abs = Math.abs;" :: Number -> Number
 
-foreign import acos
-  "function acos(n){\
-  \  return Math.acos(n);\
-  \}" :: Number -> Number
+foreign import acos "var acos = Math.acos;" :: Number -> Number
 
-foreign import asin
-  "function asin(n){\
-  \  return Math.asin(n);\
-  \}" :: Number -> Number
+foreign import asin "var asin = Math.asin;" :: Number -> Number
 
-foreign import atan
-  "function atan(n){\
-  \  return Math.atan(n);\
-  \}" :: Number -> Number
+foreign import atan "var atan = Math.atan;" :: Number -> Number
 
 foreign import atan2
   "function atan2(y){\
@@ -27,30 +15,15 @@ foreign import atan2
   \  };\
   \}" :: Number -> Number -> Number
 
-foreign import aceil
-  "function aceil(n){\
-  \  return Math.aceil(n);\
-  \}" :: Number -> Number
+foreign import ceil "var ceil = Math.ceil;" :: Number -> Number
 
-foreign import cos
-  "function cos(n){\
-  \  return Math.cos(n);\
-  \}" :: Number -> Number
+foreign import cos "var cos = Math.cos;" :: Number -> Number
 
-foreign import exp
-  "function exp(n){\
-  \  return Math.exp(n);\
-  \}" :: Number -> Number
+foreign import exp "var exp = Math.exp;" :: Number -> Number
 
-foreign import floor
-  "function floor(n){\
-  \  return Math.floor(n);\
-  \}" :: Number -> Number
+foreign import floor "var floor = Math.floor;" :: Number -> Number
 
-foreign import log
-  "function log(n){\
-  \  return Math.log(n);\
-  \}" :: Number -> Number
+foreign import log "var log = Math.log;" :: Number -> Number
 
 foreign import max
   "function max(n1){\
@@ -73,25 +46,13 @@ foreign import pow
   \  }\
   \}" :: Number -> Number -> Number
 
-foreign import round
-  "function round(n){\
-  \  return Math.round(n);\
-  \}" :: Number -> Number
+foreign import round "var round = Math.round;" :: Number -> Number
 
-foreign import sin
-  "function sin(n){\
-  \  return Math.sin(n);\
-  \}" :: Number -> Number
+foreign import sin "var sin = Math.sin;" :: Number -> Number
 
-foreign import sqrt
-  "function sqrt(n){\
-  \  return Math.sqrt(n);\
-  \}" :: Number -> Number
+foreign import sqrt "var sqrt = Math.sqrt;" :: Number -> Number
 
-foreign import tan
-  "function tan(n){\
-  \  return Math.tan(n);\
-  \}" :: Number -> Number
+foreign import tan "var tan = Math.tan;" :: Number -> Number
 
 foreign import e       "var e       = Math.E;"       :: Number
 foreign import ln2     "var ln2     = Math.LN2;"     :: Number
