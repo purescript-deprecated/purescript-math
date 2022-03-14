c052d221193d25255f6b9024df8df174e54dc550
JordanMartinez <jordanalex.martinez@protonmail.com>
2022-03-14 06:48:59 -0700

    Update to v0.15.0 (#33)
    * Convert foreign modules to try bundling with esbuild
    
    * Update .eslintrc.json to ES6
    
    * Replaced 'export var' with 'export const'
    
    * Removed '"use strict";' in FFI files
    
    * Update to CI to use 'unstable' purescript
    
    * Update pulp and psa
    
    * Update changelog
    
    Co-authored-by: Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>

--- src/Math.js
+++ src/Math.js
@@ -1,28 +1,26 @@
-"use strict";
-
 // module Math
 
-exports.abs = Math.abs;
+export const abs = Math.abs;
 
-exports.acos = Math.acos;
+export const acos = Math.acos;
 
-exports.asin = Math.asin;
+export const asin = Math.asin;
 
-exports.atan = Math.atan;
+export const atan = Math.atan;
 
-exports.atan2 = function (y) {
+export const atan2 = function (y) {
   return function (x) {
     return Math.atan2(y, x);
   };
 };
 
-exports.ceil = Math.ceil;
+export const ceil = Math.ceil;
 
-exports.cos = Math.cos;
+export const cos = Math.cos;
 
-exports.exp = Math.exp;
+export const exp = Math.exp;
 
-exports.floor = Math.floor;
+export const floor = Math.floor;
 
 function nativeImul(a) {
   return function (b) {
@@ -44,60 +42,60 @@ function emulatedImul(a) {
   };
 }
 
-exports.imul = Math.imul ? nativeImul : emulatedImul;
+export const imul = Math.imul ? nativeImul : emulatedImul;
 
-exports.trunc = Math.trunc || function (n) {
+export const trunc = Math.trunc || function (n) {
   return n < 0 ? Math.ceil(n) : Math.floor(n);
 };
 
-exports.log = Math.log;
+export const log = Math.log;
 
-exports.max = function (n1) {
+export const max = function (n1) {
   return function (n2) {
     return Math.max(n1, n2);
   };
 };
 
-exports.min = function (n1) {
+export const min = function (n1) {
   return function (n2) {
     return Math.min(n1, n2);
   };
 };
 
-exports.pow = function (n) {
+export const pow = function (n) {
   return function (p) {
     return Math.pow(n, p);
   };
 };
 
-exports.remainder = function (n) {
+export const remainder = function (n) {
   return function (m) {
     return n % m;
   };
 };
 
-exports.round = Math.round;
+export const round = Math.round;
 
-exports.sin = Math.sin;
+export const sin = Math.sin;
 
-exports.sqrt = Math.sqrt;
+export const sqrt = Math.sqrt;
 
-exports.tan = Math.tan;
+export const tan = Math.tan;
 
-exports.e = Math.E;
+export const e = Math.E;
 
-exports.ln2 = Math.LN2;
+export const ln2 = Math.LN2;
 
-exports.ln10 = Math.LN10;
+export const ln10 = Math.LN10;
 
-exports.log2e = Math.LOG2E;
+export const log2e = Math.LOG2E;
 
-exports.log10e = Math.LOG10E;
+export const log10e = Math.LOG10E;
 
-exports.pi = Math.PI;
+export const pi = Math.PI;
 
-exports.tau = 2 * Math.PI;
+export const tau = 2 * Math.PI;
 
-exports.sqrt1_2 = Math.SQRT1_2;
+export const sqrt1_2 = Math.SQRT1_2;
 
-exports.sqrt2 = Math.SQRT2;
+export const sqrt2 = Math.SQRT2;
